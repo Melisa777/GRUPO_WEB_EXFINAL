047d51f7be86b25e56bc49ae1d58661cab71b291
Melisa777 <iepvccardenas@gmail.com>
2017-07-22 12:23:50 -0500

    Estructura

---
+++ EXAMEN_FINAL_CINE/EXAMEN_FINAL_CINE/Areas/ACine/ACineAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace EXAMEN_FINAL_CINE.Areas.ACine
+{
+    public class ACineAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ACine";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ACine_default",
+                "ACine/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ EXAMEN_FINAL_CINE/EXAMEN_FINAL_CINE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EXAMEN_FINAL_CINE.Startup))]
+namespace EXAMEN_FINAL_CINE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
