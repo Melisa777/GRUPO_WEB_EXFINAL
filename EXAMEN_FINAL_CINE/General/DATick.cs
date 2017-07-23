0fffbdd96e4d1d7030c7b38b8f95576b2a7c4d9c
Melisa777 <iepvccardenas@gmail.com>
2017-07-22 20:15:45 -0500

    agregando ticket

---
+++ EXAMEN_FINAL_CINE/General/DATick.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General
+{
+    class DATick
+    {
+        static public List<Ticket> ListadoDirector()
+        {
+
+            List<Ticket> alumno = new List<Ticket>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Ticket.ToList();
+            }
+        }
+    }
+}
