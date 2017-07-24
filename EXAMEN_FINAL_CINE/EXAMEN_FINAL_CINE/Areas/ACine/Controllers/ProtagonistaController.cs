00e80446d39afcc4172ec18f137df1d0ae1360a4
Melisa <iepvccardenas@gmail.com>
2017-07-24 12:55:19 -0500

    CRUD Protagonista

---
+++ EXAMEN_FINAL_CINE/EXAMEN_FINAL_CINE/Areas/ACine/Controllers/ProtagonistaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using General;
+
+namespace EXAMEN_FINAL_CINE.Areas.ACine.Controllers
+{
+    public class ProtagonistaController : Controller
+    {
+        // GET: ACine/Protagonista
+        // GET: ACine/CinePelicula
+        public ActionResult Index()
+        {
+            ViewBag.ListadoProtagonista = DAProtagonista.ListadoProtagonista();
+            ViewBag.ListadoInterprete = DAInterprete.ListadoInterprete();
+            ViewBag.ListadoPelicula = DAPELICULA.ListadoPelicula();
+            return View();
+        }
+        public ActionResult Registro()
+        {
+            ViewBag.ListadoProtagonista = DAProtagonista.ListadoProtagonista();
+            ViewBag.ListadoInterprete = DAInterprete.ListadoInterprete();
+            ViewBag.ListadoPelicula = DAPELICULA.ListadoPelicula();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registro(General.Protagonista protagonista)
+        {
+            //para cargar la data
+            bool exito = DAProtagonista.RegistrarProtagonista(protagonista);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        public ActionResult Eliminar(int ID)
+        {
+            bool exito = DAProtagonista.EliminarProtagonista(ID);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int ID)
+        {
+            General.Protagonista PELI = DAProtagonista.ListadoProtagonista().FirstOrDefault();
+            return View(PELI);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(General.Protagonista PRO)
+        {
+            //para cargar la data
+            bool exito = DAProtagonista.ActualizarProtagonista(PRO);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+
+
+}
---
+++ EXAMEN_FINAL_CINE/General/DAProtagonista.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace General
+{
+    public class DAProtagonista
+    {
+
+        static public List<Protagonista> ListadoProtagonista()
+        {
+            List<Protagonista> listado = new List<Protagonista>();
+            using (var data = new CineDBEntities())
+            {
+                return data.Protagonista.ToList();
+            }
+        }
+
+        static public List<Pelicula> ListadoPelicula()
+        {
+            using (var data = new CineDBEntities())
+            {
+                return data.Pelicula.ToList();
+            }
+
+        }
+
+        static public List<Interprete> ListadoInterprete()
+        {
+            using (var data = new CineDBEntities())
+            {
+                return data.Interprete.ToList();
+            }
+
+        }
+
+        //
+
+        static public bool RegistrarProtagonista(Protagonista protagonista)
+        {
+            bool exito = true;
+
+            try
+            {
+                using (var data = new CineDBEntities())
+                {
+                    data.Protagonista.Add(protagonista);
+                    data.SaveChanges();
+                }
+            }
+            catch
+            {
+                exito = false;
+            }
+
+            return exito;
+        }
+
+        static public bool ActualizarProtagonista(Protagonista cinePeli) // referido a objeto
+        {
+            bool exito = true; // validación del registro
+            // para el manejo de excepciones
+            try
+            {
+                using (var data = new CineDBEntities())
+                {
+                    // realizar la consulta y actualizar
+                    Protagonista actual = data.Protagonista.Where(x => x.NombreProtagonista == cinePeli.NombreProtagonista).FirstOrDefault();// alias
+                    
+                    actual.EsPrincipal = cinePeli.EsPrincipal;
+               
+                    data.SaveChanges(); // guarda los cambios
+                }
+            }
+            catch (Exception)
+            {
+                // cuando ocurre el error
+                exito = false;
+            }
+            return exito;
+
+        }
+
+        static public bool EliminarProtagonista(int CodCine) // referido a borrar
+        {
+            bool exito = true;
+            try
+            {
+
+                using (var data = new CineDBEntities())
+                {
+                    Protagonista actual = data.Protagonista.Where(z => z.CodInterprete == CodCine).FirstOrDefault();// alias
+                    data.Protagonista.Remove(actual);
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+
+            return exito;
+        }
+
+    }
+
+
+}
+}
+
