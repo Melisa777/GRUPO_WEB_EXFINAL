b62c707051cba844a4d3868b6dc28fe2793e0600
Melisa <iepvccardenas@gmail.com>
2017-07-24 13:31:12 -0500

    CRUD_ticket

---
+++ EXAMEN_FINAL_CINE/EXAMEN_FINAL_CINE/Areas/ACine/Controllers/TicketController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using General;
+
+namespace EXAMEN_FINAL_CINE.Areas.ACine.Controllers
+{
+    public class TicketController : Controller
+    {
+        // GET: ACine/Ticket
+        public ActionResult Index()
+        {
+
+            ViewBag.ListadoTicket = DATicket.ListadoTicket();
+            ViewBag.ListadoAsiento = DAAsiento.ListadoAsiento();
+            ViewBag.ListadoPersona = DAPersonaa.ListadoPersona();
+            ViewBag.ListadoFuncion = DAFuncion.ListadoFuncion();
+
+            return View();
+        }
+        public ActionResult Registro()
+        {
+
+            ViewBag.ListadoTicket = DATicket.ListadoTicket();
+            return View();
+        }
+    
+        [HttpPost]
+        public ActionResult Registro(General.Ticket ticket)
+        {
+            //para cargar la data
+            bool exito = DATicket.RegistrarTicket(ticket);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        public ActionResult Eliminar(int ID)
+        {
+            bool exito = DATicket.EliminarTicket(ID);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int ID)
+        {
+            General.Ticket ti = DATicket.ListadoTicket().FirstOrDefault();
+            return View(ti);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(General.Ticket ticket)
+        {
+            //para cargar la data
+            bool exito = DATicket.ActualizarTicket(ticket);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
--- EXAMEN_FINAL_CINE/General/DATicket.cs
+++ EXAMEN_FINAL_CINE/General/DATicket.cs
@@ -6,7 +6,92 @@ using System.Threading.Tasks;
 
 namespace General
 {
-    class DATicket
+   public  class DATicket
     {
+        static public List<Ticket> ListadoTicket()
+        {
+            List<Ticket> listado = new List<Ticket>();
+
+            using (var data = new CineDBEntities())
+            {
+                listado = data.Ticket.ToList();
+            }
+
+            return listado;
+        }
+
+
+
+
+        static public bool RegistrarTicket(Ticket ticket)
+        {
+            bool exito = true;
+
+            try
+            {
+                using (var data = new CineDBEntities())
+                {
+                    data.Ticket.Add(ticket);
+                    data.SaveChanges();
+                }
+            }
+            catch
+            {
+                exito = false;
+            }
+
+            return exito;
+        }
+
+
+        static public bool ActualizarTicket(Ticket ticket) // referido a objeto
+        {
+            bool exito = true; // validación del registro
+                               // para el manejo de excepciones
+            try
+            {
+                using (var data = new CineDBEntities())
+                {
+                    // realizar la consulta y actualizar
+                    Ticket actual = data.Ticket.Where(x => x.CodTicket == ticket.CodTicket).FirstOrDefault();// alias
+                                                                                                                       //actual.ID_Empresa = empresa.ID_Empresa;
+                                                                                                                       //actual.ID_Usuario = empresa.ID_Usuario;
+                    actual.Descripcion = ticket.Descripcion;
+                    actual.CodAsiento = ticket.CodAsiento;
+                    actual.CodPersona = ticket.CodPersona;
+                    actual.CodFuncion = ticket.CodFuncion;
+                    data.SaveChanges(); // guarda los cambios
+                }
+            }
+            catch (Exception)
+            {
+                // cuando ocurre el error
+                exito = false;
+            }
+            return exito;
+
+        }
+
+        static public bool EliminarTicket(int CodTicket) // referido a borrar
+        {
+            bool exito = true;
+            try
+            {
+
+                using (var data = new CineDBEntities())
+                {
+                    Ticket actual = data.Ticket.Where(z => z.CodTicket == CodTicket).FirstOrDefault();// alias
+                    data.Ticket.Remove(actual);
+                    data.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                exito = false;
+            }
+
+            return exito;
+        }
     }
 }
